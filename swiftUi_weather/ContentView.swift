abada8955572b7bce521c8bcb880d071c2f17506
anvesh <anveshmishra25@gmail.com>
2026-03-03 15:46:44 +0530

    Initial Commit

---
+++ swiftUi_weather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftUi_weather
+//
+//  Created by Anvesh Mishra on 03/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftUi_weather/swiftUi_weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUi_weatherApp.swift
+//  swiftUi_weather
+//
+//  Created by Anvesh Mishra on 03/03/26.
+//
+
+import SwiftUI
+
+@main
+struct swiftUi_weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
